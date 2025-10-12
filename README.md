9fef61bcbfdc2c4961b1aaef3e496b3886c47d30
Allan Clark <allanc@chickenandpork.com>
2025-10-11 23:58:46 -0700

    feat: upscale to python3

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# pydhcplib
+# pydhcplib3
 
 Pydhcplib is a python library to read/write and encode/decode dhcp
-packet on network. 
+packet on network.
 
 ##Installation
 
 Simply run `python setup.py install`. Python modules will be
-installed in /usr/lib/python2.X/site-packages/pydhcplib/. 
+installed in /usr/lib/python3.X/site-packages/pydhcplib3/.
 
 
-## How to use pydhcplib
+## How to use pydhcplib3
 
 Look in the examples directory to learn how to use the modules.
 
 `man pydhcp`
 
-`man pydhcplib`
+`man pydhcplib3`
--- scripts/pydhcp.py
+++ scripts/pydhcp.py
@@ -1,67 +1,103 @@
 #!/usr/bin/env python
 
-
-import dhcp_constants
-import dhcp_packet
-import dhcp_network
-
-import type_hw_addr
-import type_ipv4
-import type_strlist
+# import dhcp_constants
+# import dhcp_network
+# import type_hw_addr
+# import type_ipv4
+# import type_strlist
 
 import sys
-
+import dhcp_packet
 from optparse import OptionParser
 
 
 parser = OptionParser()
 
 """ Action options """
-parser.add_option("-L", "--listen", action="store_true",dest="listen", help="",default="False")
-parser.add_option("-E", "--emit",  action="store_true",dest="emit", help="",  default="False")
-parser.add_option("-R", "--readable-conversion",  action="store_true",dest="readable", help="", default="False")
-parser.add_option("-B", "--binary-conversion",  action="store_true",dest="binary", help="", default="False")
+parser.add_option(
+    "-L", "--listen", action="store_true", dest="listen", help="", default="False"
+)
+parser.add_option(
+    "-E", "--emit", action="store_true", dest="emit", help="", default="False"
+)
+parser.add_option(
+    "-R",
+    "--readable-conversion",
+    action="store_true",
+    dest="readable",
+    help="",
+    default="False",
+)
+parser.add_option(
+    "-B",
+    "--binary-conversion",
+    action="store_true",
+    dest="binary",
+    help="",
+    default="False",
+)
 
 
-parser.add_option("-s", "--source-file", action="store",dest="source", help="", default="False", type="string")
-parser.add_option("-d", "--destination-file", action="store",dest="destination", help="", default="False", type="string")
-
+parser.add_option(
+    "-s",
+    "--source-file",
+    action="store",
+    dest="source",
+    help="",
+    default="False",
+    type="string",
+)
+parser.add_option(
+    "-d",
+    "--destination-file",
+    action="store",
+    dest="destination",
+    help="",
+    default="False",
+    type="string",
+)
 
 
 (options, args) = parser.parse_args()
 
-print options
+print(options)
 
 
-
-def main() :
+def main():
     ActionSum = 0
-    for Action in (options.listen,options.emit,options.readable,options.binary) :
-        if Action == True : ActionSum += 1
-    if ActionSum > 1 :
-        print "Command line error : [-L -E -R -B] Only one of these actions can be taken."
+    for Action in (options.listen, options.emit, options.readable, options.binary):
+        if Action:
+            ActionSum += 1
+    if ActionSum > 1:
+        print(
+            "Command line error : [-L -E -R -B] Only one of these actions can be taken."
+        )
         sys.exit(0)
 
-    if options.readable == True : r_conversion()
+    if options.readable:
+        r_conversion()
 
 
-
-def listen(port) :
+def listen(port):
     pass
 
-def emit(address,port) :
+
+def emit(address, port):
     pass
 
-def r_conversion() :
+
+def r_conversion():
     rawdata = rawdata = sys.stdin.read()
-    while ( len(rawdata)>0 ) :
+    while len(rawdata) > 0:
         readdata = dhcp_packet.DhcpPacket()
         readdata.DecodePacket(rawdata)
         readdata.PrintHeaders()
         readdata.PrintOptions()
         rawdata = rawdata = sys.stdin.read()
 
-def b_conversion() :
+
+def b_conversion():
     pass
+
 
 main()
